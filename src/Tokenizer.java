4709a83a1ba447ea9bd0f00ccc9f4e8c4bb5d39d
JKTKops <zerglingk9012@gmail.com>
2018-10-05 17:21:14 -0500

    Tokenizer now works.

--- src/Tokenizer.java
+++ src/Tokenizer.java
@@ -1,14 +1,23 @@
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.regex.Pattern;
 
 public class Tokenizer {
     public static void main(String[] args) {
+        File f = new File("C:/Users/zergl/IdeaProjects/JBGPL Compiler/src/files/Jack Files/Square/Square.jack");
+        String[] tokens = tokenize(f);
+        
+    }
+
+    public static String[] tokenize(File file) {
         BufferedReader br;
         String fileText;
         StringBuilder sb = new StringBuilder();
         try{
-            br = new BufferedReader(new FileReader("C:/Users/zergl/IdeaProjects/JBGPL Compiler/src/files/Jack Files/Square/Square.jack"));
+            br = new BufferedReader(new FileReader(file));
             String s;
             while ((s = br.readLine()) != null) {
                 sb.append(s);
@@ -21,14 +30,24 @@ public class Tokenizer {
 
         String comments = "(//.*$)|(/\\*(.|\\n)*?\\*/)";
         String symbols = "[<>=^~|&+\\-/.,;{}\\[\\]()]";
+
+        /* delete comments from the input string */
         fileText = Pattern.compile(comments, Pattern.MULTILINE).matcher(fileText).replaceAll("");
-        fileText = fileText.replaceAll("\\r\\n", "");
-        String[] tokens = Pattern.compile("(?<="+symbols+")|((?="+symbols+")\\s*)|(\\s+)").split(fileText);
-        for (String s: tokens) {
-            s = s.trim();
-            if (s.length() > 0) {
-                System.out.println(s);
+
+        /* split the input string into tokens and store in ArrayList */
+        ArrayList<String> tokens = new ArrayList<>(Arrays.asList(Pattern.compile("(?<="+symbols+")|((?="+symbols+")\\s*)|(\\s+)").split(fileText)));
+
+        // unfortunately without tripling the length of this regex,
+        // we can't avoid having a number of pure-whitespace strings in our list.
+        for (int i = 0; i < tokens.size();) {
+            if (tokens.get(i).trim().length() == 0) {
+                tokens.remove(i);
+            } else {
+                tokens.add(i, tokens.remove(i).trim());
+                i++;
             }
         }
+
+        return tokens.toArray(new String[0]);
     }
 }
